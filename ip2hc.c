1521b24d8811c0bd0f4b4b390d41e717edc3ae3d
Alisha-Rath <alisharath0@gmail.com>
2017-02-16 23:52:23 +0530

    Update ip2hc.c

--- ip2hc.c
+++ ip2hc.c
@@ -5,7 +5,17 @@
 #include <linux/udp.h>
 #include <linux/ip.h>
 #define BUFFERSIZE 10
-struct pair ip2hc
+
+struct pair 
 {
+  unsigned int hcount;
+  unsigned int ip;
+}ip2hc;
   //todo
-}
+  /*1. collect traces of its clients to obtain both IP ad-dresses and the corresponding hop-count values
+    2.After the initial population of the mapping table and activation,
+    HCF will continue adding new entries to the mapping table
+    when requests with previously unseen legitimate IP addresses
+    are sighted
+   */
+
