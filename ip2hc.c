23b52850a94b90f50322fdee63d65798831bb5dc
Alisha-Rath <alisharath0@gmail.com>
2017-02-16 23:39:46 +0530

    Create ip2hc.c

---
+++ ip2hc.c
@@ -0,0 +1,8 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/udp.h>
+#include <linux/ip.h>
+#define BUFFERSIZE 10
+
